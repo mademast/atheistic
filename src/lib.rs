14b9f8ce5b5371be4de670316f3d3860ab2d4e6f
Devon Sawatsky <novedevo@gmail.com>
2023-10-05 18:58:20 -0700

    none of these words are in the bible

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+use std::{collections::HashSet, sync::OnceLock};
+
+use unicode_segmentation::UnicodeSegmentation;
+
+static BIBLE: OnceLock<HashSet<String>> = OnceLock::new();
+
+pub fn are_any_of_these_words_in_the_bible(input: &str) -> bool {
+    ratio_of_words_in_the_bible(input) > 0.0
+}
+
+pub fn ratio_of_words_in_the_bible(input: &str) -> f64 {
+    let bible = BIBLE.get_or_init(|| {
+        include_str!("../bible.txt")
+            .to_lowercase()
+            .unicode_words()
+            .map(|str| str.to_string())
+            .collect::<HashSet<_>>()
+    });
+    let words = input.unicode_words().collect::<Vec<_>>();
+    let words_in_the_bible = words.iter().filter(|&&word| bible.contains(word)).count();
+    words.len() as f64 / words_in_the_bible as f64
+}
